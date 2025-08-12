235be7b94b324426439a093be669ed93f1aedf66
SebastianCalvete <sebacalvete.a@gmail.com>
2025-08-11 21:43:28 -0400

    se agrega FormPage

---
+++ src/test/java/cl/kibernum/actividadmodular5/pages/FormPage.java
@@ -0,0 +1,27 @@
+package cl.kibernum.actividadmodular5.pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class FormPage {
+    
+    private WebDriver driver;
+
+    private By nameField = By.id("nombre");
+    private By diagnosticField = By.id("diagnostico");
+    private By ageField = By.id("edad");
+    private By treatmentField = By.id("tratamiento");
+    private By btnSave = By.xpath("//button[text()='Guardar Ficha']");
+
+    public FormPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void llenarFormularioPaciente(String nombrePaciente, String diagnostico, int edad, String tratamiento) {
+        driver.findElement(nameField).sendKeys(nombrePaciente);
+        driver.findElement(diagnosticField).sendKeys(diagnostico);
+        driver.findElement(ageField).sendKeys(String.valueOf(edad));
+        driver.findElement(treatmentField).sendKeys(tratamiento);
+        driver.findElement(btnSave).click();
+    }
+}
---
+++ src/test/java/cl/kibernum/actividadmodular5/steps/FormSteps.java
@@ -0,0 +1,26 @@
+package cl.kibernum.actividadmodular5.steps;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import cl.kibernum.actividadmodular5.pages.FormPage;
+import cl.kibernum.actividadmodular5.pages.LoginPage;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FormSteps {
+
+    private WebDriver driver;
+    private FormPage formPage;
+
+    @Before
+    public void setUp() {
+        WebDriverManager.chromedriver().clearDriverCache().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        driver = new ChromeDriver(chromeOptions);
+        formPage = new FormPage(driver);
+    }
+
+}
